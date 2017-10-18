615f34496d733beb0c054dbaab0b3da252608318
Arcadia2105380 <32221009+Arcadia2105380@users.noreply.github.com>
2017-10-18 10:52:40 +0800

    Update change_color.js

--- change_color.js
+++ change_color.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $("div").click(function(){
         $("p").css("color", "blue");
+        $("p").hide();
     });
     $("p").click(function(){
         $(this).hide();
