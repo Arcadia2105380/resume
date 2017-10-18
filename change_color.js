dfca18e6401c3b5bb2a7daa9dda845917342e39d
Arcadia2105380 <32221009+Arcadia2105380@users.noreply.github.com>
2017-10-18 11:38:28 +0800

    Update change_color.js

--- change_color.js
+++ change_color.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
         val color = this.background;
         $("h1").css("color", color);
     });
+    $("green").click(function(){
+          $("p").css("color", "green");
+     });
+     $("red").click(function(){
+         $("p").css("color", "red");
+     });
     $("p").click(function(){
         $(this).hide();
     });
